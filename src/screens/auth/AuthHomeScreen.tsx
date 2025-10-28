a4fad8b7a6483d0a26abd3e672e616938b6b2d2d
신준혁 <oridori2705@naver.com>
2025-10-28 20:42:47 +0900

    feat: 네비게이션 세팅 및 react-native-safe-area-context 설치

---
+++ src/screens/auth/AuthHomeScreen.tsx
@@ -0,0 +1,25 @@
+import {useNavigation} from '@react-navigation/native';
+import {Pressable, StyleSheet, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+function AuthHomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView>
+      <Pressable onPress={() => navigation.navigate('Login')}>
+        <Text>로그인으로 이동</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff', // (선택) 배경색 지정
+    paddingTop: 0, // SafeAreaView가 상태바 높이를 자동으로 확보함
+  },
+});
+
+export default AuthHomeScreen;
