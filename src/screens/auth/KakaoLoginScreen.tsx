7c6737a9904b3a305326f82262f26828ae6204ed
신준혁 <oridori2705@naver.com>
2025-11-11 18:30:17 +0900

    fix: 누락 부분 추가

---
+++ src/screens/auth/KakaoLoginScreen.tsx
@@ -0,0 +1,68 @@
+import Indicator from '@/component/common/Indicator';
+import useAuth from '@/hooks/queries/useAuth';
+import axios from 'axios';
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
+import Config from 'react-native-config';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import WebView, {WebViewNavigation} from 'react-native-webview';
+
+const REDIRECT_URI = `http://localhost:3030/auth/oauth/kakao`;
+
+function KakaoLoginScreen() {
+  const {kakaoLoginMutation} = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const requestToken = async (code: string) => {
+    const response = await axios({
+      method: 'post',
+      url: 'https://kauth.kakao.com/oauth/token',
+      params: {
+        grant_type: 'authorization_code',
+        client_id: Config.KAKAO_REST_API_KEY,
+        redirect_uri: REDIRECT_URI,
+        code,
+      },
+    });
+
+    kakaoLoginMutation.mutate(response.data.access_token);
+  };
+
+  /**
+   * 🌐 WebView의 URL 이동 이벤트 감지 핸들러
+   * 카카오 로그인 성공 시 redirect_uri로 리디렉션되며 URL에 code가 포함됨
+   */
+  const handleShouldStartLoadWithRequest = (event: WebViewNavigation) => {
+    const isMatched = event.url.includes(`${REDIRECT_URI}?code=`);
+
+    if (isMatched) {
+      const code = event.url.replace(`${REDIRECT_URI}?code=`, '');
+      requestToken(code);
+      setIsLoading(event.loading);
+      return false;
+    }
+
+    return true;
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {isLoading && <Indicator size="large" />}
+      <WebView
+        style={styles.container}
+        source={{
+          uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${Config.KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
+        }}
+        onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default KakaoLoginScreen;
